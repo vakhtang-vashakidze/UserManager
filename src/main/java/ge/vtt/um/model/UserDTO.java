e809184a27fc5e03420a8b679454c855acdc35e7
vakhtang-vashakidze <vvash2017@gmail.com>
2022-09-13 17:35:16 +0400

    added spring validation and used the functionality in the dto

--- src/main/java/ge/vtt/um/model/UserDTO.java
+++ src/main/java/ge/vtt/um/model/UserDTO.java
@@ -2,11 +2,19 @@ package ge.vtt.um.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UserDTO {
+    @NotBlank
     private String username;
+    @NotBlank
     private String firstname;
+    @NotBlank
     private String lastname;
+    @Email
     private String email;
+    @NotBlank
     private String password;
 }
