564cd11a03744a5e2cac3e901503f0b9ac881d88
vakhtang-vashakidze <vvash2017@gmail.com>
2022-09-13 21:41:00 +0400

    added general response to health controller

--- src/main/java/ge/vtt/um/controller/HealthController.java
+++ src/main/java/ge/vtt/um/controller/HealthController.java
@@ -1,5 +1,7 @@
 package ge.vtt.um.controller;
 
+import ge.vtt.um.model.transfer.GeneralResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/heartbeat")
-    public ResponseEntity<String> heartbeat() {
-        return ResponseEntity.ok("I am running!");
+    public ResponseEntity<GeneralResponse> heartbeat() {
+        return ResponseEntity.ok(GeneralResponse.builder().message("I am working!").status(HttpStatus.OK.value()).build());
     }
 }
